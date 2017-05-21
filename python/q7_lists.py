b73a0822c2ff6cca54b10bce05c8fe2199aa2d9c
Joseph Eddy <jle2129@columbia.edu>
2017-05-21 14:19:53 -0400

    Update q7_lists.py
    Finished q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -7,7 +7,6 @@ def match_ends(words):
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
     of the string are the same.
-
     >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
     3
     >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
@@ -15,7 +14,12 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    
+    count = 0
+    for s in words:
+        if (len(s) > 1 and s[0] == s[-1]):
+            count += 1
+    return count
 
 
 def front_x(words):
@@ -24,7 +28,6 @@ def front_x(words):
     order, except group all the strings that begin with 'x' first.
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
     >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
     ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
     >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
@@ -32,7 +35,14 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    
+    x_words = []
+    other_words = []
+    for i in range(0,len(words)):
+        if words[i][0] == 'x':
+            x_words.append(words[i])
+        else: other_words.append(words[i])
+    return sorted(x_words) + sorted(other_words)
 
 
 def sort_last(tuples):
@@ -41,7 +51,6 @@ def sort_last(tuples):
     increasing order by the last element in each tuple.
     e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
     >>> sort_last([(1, 3), (3, 2), (2, 1)])
     [(2, 1), (3, 2), (1, 3)]
     >>> sort_last([(2, 3), (1, 2), (3, 1)])
@@ -49,7 +58,8 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    
+    return sorted(tuples, key = lambda tup: tup[-1])
 
 
 def remove_adjacent(nums):
@@ -58,7 +68,6 @@ def remove_adjacent(nums):
     elements have been reduced to a single element, so [1, 2, 2, 3]
     returns [1, 2, 3]. You may create a new list or modify the passed
     in list.
-
     >>> remove_adjacent([1, 2, 2, 3])
     [1, 2, 3]
     >>> remove_adjacent([2, 2, 3, 3, 3])
@@ -68,7 +77,15 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+
+    if len(nums) == 0:
+        return nums
+    
+    reduced_nums = [nums[0]]
+    for i in range(1,len(nums)):
+        if nums[i] != nums[i-1]:
+            reduced_nums.append(nums[i])
+    return reduced_nums
 
 
 def linear_merge(list1, list2):
@@ -77,7 +94,6 @@ def linear_merge(list1, list2):
     merged list of all the elements in sorted order. You may modify
     the passed in lists. Ideally, the solution should work in "linear"
     time, making a single pass of both lists.
-
     >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
     ['aa', 'bb', 'cc', 'xx', 'zz']
     >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
@@ -85,4 +101,25 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    
+    list_merged = []
+    i, j = 0, 0
+    
+    while (not (i == len(list1) and j == len(list2))):
+        
+        if i == len(list1):
+            list_merged.extend(list2[j:])
+            return list_merged
+        if j == len(list2):
+            list_merged.extend(list1[i:])
+            return list_merged
+        
+        if list1[i] <= list2[j]:
+            list_merged.append(list1[i])
+            i += 1
+        else:
+            list_merged.append(list2[j])
+            j += 1
+            
+        
+            
